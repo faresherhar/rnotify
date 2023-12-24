5c32600964f58cb65f817a9249eeb14a3e512b9a
Herhar Fares <none>
2023-12-24 20:08:01 +0100

    refactor

--- src/scraper/utils/repos_validator.py
+++ src/scraper/utils/repos_validator.py
@@ -4,7 +4,7 @@ from schema import Schema, SchemaError, Regex, Optional
 config_schema = Schema({Optional("github"): [Regex(r"^[\w\.-]+\/[\w\.-]+$")]})
 
 
-def valid_config_file(config_dict: dict[str, object]) -> bool:
+def validate_repos(config_dict: dict[str, list[str]]) -> (bool, SchemaError | None):
     try:
         config_schema.validate(config_dict)
         return True, None
--- tests/test_repos_validator.py
+++ tests/test_repos_validator.py
@@ -1,15 +1,15 @@
 from yaml import safe_load
 
-from src.scraper.utils.repos_validator import valid_config_file
+from src.scraper.utils.repos_validator import validate_repos
 
 
 def test_good_repos():
     with open("samples/good_repos.yaml", "r") as file:
         config_dict = safe_load(file)
-        assert valid_config_file(config_dict=config_dict)[0] is True
+        assert validate_repos(config_dict=config_dict)[0] is True
 
 
 def test_bad_repos():
     with open("samples/bad_repos.yaml", "r") as file:
         config_dict = safe_load(file)
-        assert valid_config_file(config_dict=config_dict)[0] is False
+        assert validate_repos(config_dict=config_dict)[0] is False
