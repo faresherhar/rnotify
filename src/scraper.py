13eb7f2ea80f4c12f1828cdec48123292fd930a8
Herhar Fares <none>
2024-01-29 22:27:42 +0100

    fix diagram

--- src/scraper.py
+++ src/scraper.py
@@ -22,55 +22,56 @@ def get_db_session():
         db_session.close()
 
 
-# Define logger
-logger = logging.getLogger(__name__)
+if __name__ == '__main__':
+    # Define logger
+    logger = logging.getLogger(__name__)
+    
+    # Create Tables [IF NOT EXISTS]
+    logger.info("Initiating Tables Creation")
+    Repo.metadata.create_all(bind=engine)
 
-# Create Tables [IF NOT EXISTS]
-logger.info("Initiating Tables Creation")
-Repo.metadata.create_all(bind=engine)
 
+    # Load Repos Configuration
+    with open(settings.REPOS_FILE, "r") as file:
+        # Read Repos Configuration File
+        logger.info("Loading Repos File")
+        config_dict = safe_load(file)
 
-# Load Repos Configuration
-with open(settings.REPOS_FILE, "r") as file:
-    # Read Repos Configuration File
-    logger.info("Loading Repos File")
-    config_dict = safe_load(file)
+        # Verify Empty Repos Configuration File
+        if config_dict is None:
+            logger.error(EmptyReposFileError())
+            raise EmptyReposFileError
 
-    # Verify Empty Repos Configuration File
-    if config_dict is None:
-        logger.error(EmptyReposFileError())
-        raise EmptyReposFileError
+        # Validate Repos Configuration File
+        repos_validation, err = validate_repos(config_dict=config_dict)
+        if err:
+            logger.error("Unvalid Repos File")
+            raise err
 
-    # Validate Repos Configuration File
-    repos_validation, err = validate_repos(config_dict=config_dict)
-    if err:
-        logger.error("Unvalid Repos File")
-        raise err
+    # Fetch Repos Data
+    for provider in config_dict.keys():
+        if provider == "github":
+            logger.info("Fetching Github Data")
+            for repo_name in config_dict["github"]:
+                release_body = get_github_latest_release(repo_name)
+                if release_body:
+                    add_release(
+                        provider=provider,
+                        repo_name=repo_name,
+                        tag_name=release_body["tag_name"],
+                        release_body=release_body,
+                        db_session=get_db_session(),
+                    )
 
-# Fetch Repos Data
-for provider in config_dict.keys():
-    if provider == "github":
-        logger.info("Fetching Github Data")
-        for repo_name in config_dict["github"]:
-            release_body = get_github_latest_release(repo_name)
-            if release_body:
-                add_release(
-                    provider=provider,
-                    repo_name=repo_name,
-                    tag_name=release_body["tag_name"],
-                    release_body=release_body,
-                    db_session=get_db_session(),
-                )
-
-    if provider == "gitlab":
-        logger.info("Fetching Gitlab Data")
-        for repo_name in config_dict["gitlab"]:
-            release_body = get_gitlab_latest_release(repo_name)
-            if release_body:
-                add_release(
-                    provider=provider,
-                    repo_name=repo_name,
-                    tag_name=release_body["tag_name"],
-                    release_body=release_body,
-                    db_session=get_db_session(),
-                )
+        if provider == "gitlab":
+            logger.info("Fetching Gitlab Data")
+            for repo_name in config_dict["gitlab"]:
+                release_body = get_gitlab_latest_release(repo_name)
+                if release_body:
+                    add_release(
+                        provider=provider,
+                        repo_name=repo_name,
+                        tag_name=release_body["tag_name"],
+                        release_body=release_body,
+                        db_session=get_db_session(),
+                    )
